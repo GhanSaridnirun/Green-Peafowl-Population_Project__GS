a32304cf558153f427c227ad7a4e7e96f3f13c6c
ChloeRNater <chloe.nater@nina.no>
2022-08-19 13:18:40 +0200

    Tidied up repository

---
+++ R/GPeafowlIPM_CoreModel.R
@@ -0,0 +1,203 @@
+library(nimble)
+
+mySeed <- 0
+set.seed(mySeed)
+
+
+# Data Bundle
+
+#Month order and season for data
+
+month <- factor(c("Nov19","Dec19",
+                  "Jan20","Feb20","Mar20","Apr20","May20","Jun20","Jul20","Aug20",
+                  "Sep20","Oct20","Nov20","Dec20","Jan21","Feb21","Mar21","Apr21",
+                  "May21","Jun21","Jul21","Aug21","Sep21","Oct21","Nov21","Dec21"
+),ordered = TRUE)
+
+season <- factor(c("Breeding","Breeding","Breeding","Breeding","Breeding",
+                   "Breeding","Non","Non","Non","Non","Non","Non","Breeding","Breeding",
+                   "Breeding","Breeding","Breeding","Breeding","Non","Non","Non","Non",
+                   "Non","Non","Breeding","Breeding"
+),ordered = TRUE)
+
+
+# Count in Non-Breeding Season
+
+ChF_NB_yr <- c(1,1,1,1,1,1,2,2,2,2,2,2)             #Season Label for chicks count
+ChF_NB <- c(16,26,69,80,60,20,25,24,48,59,11,12)    #Female Chicks Count
+
+Br_NB_yr <- c(1,1,1,1,1,1,2,2,2,2,2,2)              #Season Label for breeder count
+Br_NB <- c(35,34,83,78,56,25,29,28,47,57,14,8)      #Breeder count in Non-Breeding
+
+
+# Count in Breeding Season
+
+JuF_BN_yr <- c(1,1,1,1,1,1,2,2,2,2,2,2,3,3)          #Season Label for juvenile count
+JuF_BN <- c(9,18,33,33,10,13,6,9,13,27,14,25,22,0)   #Juvenile count
+
+Br_BN_yr <- c(1,1,1,1,1,1,2,2,2,2,2,2,3,3)           #Season Label for breeder count
+Br_BN <- c(9,23,32,25,12,10,14,9,11,20,13,27,0,0)    #Breeder count in Breeding
+
+
+ny=20  # Length = Number of year following Green Peafowl age
+
+gp.data <- list(
+  ChF_NB_yr=ChF_NB_yr, 
+  ChF_NB=ChF_NB, 
+  Br_NB_yr=Br_NB_yr, 
+  Br_NB=Br_NB, 
+  JuF_BN_yr=JuF_BN_yr, 
+  JuF_BN=JuF_BN, 
+  Br_BN_yr=Br_BN_yr, 
+  Br_BN=Br_BN
+)
+
+gp.constants <- list(
+  ny=ny
+)
+
+
+GP.code <- nimbleCode({
+  
+  
+  # Priors and linear models
+  # Survival
+  
+  s_NB[1] ~ dunif(0.30, 0.40)  # chick (half year)
+  s_BN[1] ~ dunif(0.60, 0.80)  # juvenile (half year)
+  
+  s_yr_sa ~ dunif(0.50, 0.70) # sub adult (whole year)
+  s_NB[2:3] <- sqrt(s_yr_sa) # sub adult (half year, N -> B)
+  s_BN[2:3] <- sqrt(s_yr_sa) # sub adult (half year, B -> N)
+  
+  s_yr_ad ~ dunif(0.60, 0.80)  # breeder (whole year)
+  s_NB[4] <- sqrt(s_yr_ad) # breeder (half year, N -> B)
+  s_BN[4] <- sqrt(s_yr_ad) # breeder (half year, B -> N)
+  
+  
+  # Productivity
+  for (t in 1:ny){
+    log.rho[t] ~ dnorm(log(mean.rho), sd = sigma.rho)
+    rho[t] <- exp(log.rho[t])
+  }
+  
+  mean.rho ~ dunif(0, 5)
+  sigma.rho ~ dunif(0, 1) 
+  
+  # Sex ratio of the chicks
+  gamma ~ dbeta(1, 1)  
+  
+  
+  # Detection Probability
+  for (t in 1:ny){
+    logit.p[t] ~ dnorm(lmean.p, sd = sigma.p)
+    p[t] <- ilogit(logit.p[t])
+  }  
+  
+  mean.p ~ dbeta(1, 1)
+  lmean.p <- logit(mean.p)
+  sigma.p ~ dunif(0, 5)
+  
+  
+  # Population count data (state-space model)
+  # Model for the initial population size in Breeding season
+  
+  for(a in 1:4){
+    pinit[a] ~ dunif(30, 200)
+    NBreed[a,1] ~ dpois(pinit[a]) 
+  }
+  
+  # Book-keeping
+  Nnon[1:4,1] <- 0
+  
+  # Process model: Breeding -> Non-Breeding season transition
+  for (t in 1:ny){
+    
+    # Total number of chicks
+    Fec[t] ~ dpois(sum(NBreed[3:4,t]) * rho[t])
+    
+    # Allocate chicks to a sex
+    NNon[1,t+1] ~ dbin(gamma, Fec[t]) # Female chicks 
+    
+    # Survival
+    for(a in 2:3){
+      NNon[a,t+1] ~ dbin(s_BN[a-1], NBreed[a-1,t])
+    }
+    
+    Nnon[4,t+1] <- surv_NBreed3[t+1] + surv_NBreed4[t+1]
+    surv_NBreed3[t+1] ~ dbin(s_BN[3], NBreed[3,t])
+    surv_NBreed4[t+1] ~ dbin(s_BN[4], NBreed[4,t])
+    
+    
+    # Process model: Non-Breeding -> Breeding season transition
+    
+    for(a in 1:4){
+      NBreed[a,t+1] ~ dbin(s_NB[a], NNon[a,t+1])
+    }
+    
+    
+    # This section I think I just forget to add p[t] in the right side 
+    # due to it would provide the estimated probability of detection
+    
+    # #Observation Model in Non-Breeding
+    # ChF_NB[t] ~ dpois(p[t] * NNon[1,t])
+    # Br_NB[t] ~ dpois(p[t] * NNon[4,t])
+    # 
+    # 
+    # #Observation Model in Breeding
+    # JuF_BN[t] ~ dpois(p[t] * NBreed[2,t])
+    # Br_BN[t] ~ dpois(p[t] * NBreed[4,t])
+    
+    
+    # # Observation Model in Non-Breeding
+    # for(j in 1:12) {
+    # 
+    #   ChF_NB[j] ~ dpois(p[t] * NNon[1,ChF_NB_yr[j]]) 
+    #   Br_NB[j] ~ dpois(p[t] * NNon[4,Br_NB_yr[j]]) 
+    # 
+    # }
+    # 
+    # # Observation Model in Breeding
+    # for(h in 1:14) {
+    # 
+    #   JuF_BN[h] ~ dpois(p[t] * NBreed[1,JuF_BN_yr[h]]) 
+    #   Br_BN[h] ~ dpois(p[t] * NBreed[4,Br_BN_yr[h]]) 
+    #   
+    # }
+  } 
+}  
+)
+
+# Initial values
+source("TestMatrixGP.R")
+
+Inits <- GP_IPM_Init(Tmax = 20)
+Inits
+
+
+# Parameters monitored
+parameters <- c("s_NB", "s_BN", "mean.rho", "gamma", "mean.p", "sigma.rho", "sigma.p", "rho", "p",
+                "NBreed", "NNon", "Fec")
+
+
+# MCMC settings
+ni <- 10
+nb <- 0
+nt <- 1
+#nc <- 3
+nc <- 1
+
+# Run
+out <- nimbleMCMC(code = GP.code,
+                  data = gp.data,
+                  constants = gp.constants,
+                  inits = Inits,
+                  monitors = parameters,
+                  samplesAsCodaMCMC = TRUE,
+                  nburnin = nb,
+                  niter = ni,
+                  thin = nt,
+                  nchains = nc)
+
+print(out)
+
---
+++ R/GPeafowlIPM_InitialSim.R
@@ -0,0 +1,139 @@
+
+GP_IPM_Init <- function(Tmax){
+  Amax <- 4
+  
+  
+  ## Set up vectors and matrices
+  NBreed <- NNon <- matrix(NA, nrow = Amax, ncol = Tmax+1)
+  s_NB <- s_BN <- rep(NA, Amax)
+  pinit <- rep(NA, Amax)
+  
+  Fec <- rep(NA, Tmax)
+  p <- logit.p <- rep(NA, Tmax) 
+  rho <- log.rho <- rep(NA, Tmax)
+  
+  surv_NBreed3 <- surv_NBreed4 <- rep(NA, Tmax+1)
+  
+  
+  ## Sample values for parameters with priors
+  # Productivity
+  mean.rho <- runif(1, 3, 5)
+  sigma.rho <- runif(1, 0, 0.1)
+  gamma <- 0.5
+  
+  
+  # Detection Probability
+  mean.p <- runif(1, 0.4, 1)
+  sigma.p <- runif(1, 0, 1)
+  
+  
+  # Initial Population Sizes
+  
+  # pinit[1:Amax] <- runif(Amax, 30, 200)
+  for(a in 1:Amax){
+    pinit[a] <- runif(1, 100, 200)
+    NBreed[a,1] <- rpois(1, pinit[a])
+  }
+  
+  NNon[1:Amax,1] <- 0
+  surv_NBreed3[1] <- surv_NBreed4[1] <- 0
+  
+  # Survival rates
+  s_NB[1] <- runif(1, 0.30, 0.40)
+  s_BN[1] <- runif(1, 0.60, 0.80)
+  
+  s_yr_sa <- runif(1, 0.50, 0.70) # sub adult (whole year)
+  s_NB[2:3] <- sqrt(s_yr_sa) # sub adult (half year, N -> B)
+  s_BN[2:3] <- sqrt(s_yr_sa) # sub adult (half year, B -> N)
+  
+  s_yr_ad <- runif(1, 0.60, 0.80)  # breeder (whole year)
+  s_NB[4] <- sqrt(s_yr_ad) # breeder (half year, N -> B)
+  s_BN[4] <- sqrt(s_yr_ad) # breeder (half year, B -> N)
+  
+  
+  
+  # Calculate vital rates
+  
+  for (t in 1:Tmax){
+    logit.p[t] <- rnorm(1, mean.p, sigma.p)
+    p[t] <- plogis(logit.p[t])
+  }  
+  
+  for (t in 1:Tmax){
+    log.rho[t] <- rnorm(1, log(mean.rho), sigma.rho)
+    rho[t] <- exp(log.rho[t])
+  }
+  
+  #----------------------------------------------------------
+  
+  # Project population size
+  
+  for(t in 1:Tmax){
+    
+    
+    # Process model: Breeding -> Non-Breeding season transition    
+    
+    # Total number of chicks
+    Fec[t] <- rpois(1, sum(NBreed[3:4,t]) * rho[t])
+    
+    # Allocate chicks to a sex
+    NNon[1,t+1] <- rbinom(1, Fec[t], gamma) # Female chicks 
+    
+    # Survival
+    for(a in 2:3){
+      NNon[a,t+1] <- rbinom(1, NBreed[a-1,t], s_BN[a-1])
+    }
+    
+    surv_NBreed3[t+1] <- rbinom(1, NBreed[3,t], s_BN[3])
+    surv_NBreed4[t+1] <- rbinom(1, NBreed[4,t], s_BN[4])
+    
+    NNon[4,t+1] <- surv_NBreed3[t+1] + surv_NBreed4[t+1]
+    
+    
+    
+    # Process model: Non-Breeding -> Breeding season transition
+    
+    for(a in 1:Amax){
+      NBreed[a,t+1] <- rbinom(1, NNon[a,t+1], s_NB[a])
+    }
+    
+    
+  }
+  
+  
+  
+  # Arrange as list and return
+  
+  Inits <- list(
+    
+    NBreed = NBreed,
+    NNon = NNon,
+    s_NB = s_NB,
+    s_BN = s_BN,
+    s_yr_sa = s_yr_sa,
+    s_yr_ad = s_yr_ad,
+    pinit = pinit,
+    Fec = Fec,
+    p = p,
+    logit.p = logit.p,
+    mean.p = mean.p,
+    sigma.p = sigma.p,
+    rho = rho,
+    log.rho = log.rho,
+    mean.rho = mean.rho,
+    sigma.rho = sigma.rho,
+    gamma = gamma,
+    surv_NBreed3 = surv_NBreed3,
+    surv_NBreed4 = surv_NBreed4
+    
+  )
+  
+  Inits
+  
+  return(Inits)
+  
+  
+}
+
+
+
