582131e3018d810e3878984738a0c03d1efce1d3
Ghan Saridnirun <g.saridnirun@gmail.com>
2022-08-30 16:15:49 +0200

    Dear Contributors
    I have edited more some of the core model following your suggestion. However, I still not sure the whole model is run correctly and proper way. I have tried to run the whole model with firstly used the initialized value and then add the survey data as well as enable the observation equations.
    
    As the model running process, I usually got these warning message:
    
    warning: logProb of data node ChF_NB[1]: logProb is -Inf.
    warning: logProb of data node ChF_NB[2]: logProb is -Inf.
    warning: logProb of data node ChF_NB[3]: logProb is -Inf.
    warning: logProb of data node ChF_NB[4]: logProb is -Inf.
    warning: logProb of data node ChF_NB[5]: logProb is -Inf.
    warning: logProb of data node ChF_NB[6]: logProb is -Inf.
    warning: logProb of data node Br_NB[1]: logProb is -Inf.
    warning: logProb of data node Br_NB[2]: logProb is -Inf.
    warning: logProb of data node Br_NB[3]: logProb is -Inf.
    warning: logProb of data node Br_NB[4]: logProb is -Inf.
    warning: logProb of data node Br_NB[5]: logProb is -Inf.
    warning: logProb of data node Br_NB[6]: logProb is -Inf.
    
    However the model could be run untill finish the process and gave me the result. These warning message also appeared when I tried to add more interaction and number of chain. The model could run trought each intercations as well as each chains.
    
    The main problem I experience currently is the parameter
    
    "NNon[4,t+1]" which alway produce the initial value causing this parameter couldn't be estimate as the model run.
    
    I suspected that error might came from this equation
    
    "Nnon[4,t+1] <- surv_NBreed3[t+1] + surv_NBreed4[t+1]
     surv_NBreed3[t+1] ~ dbin(s_BN[3], NBreed[3,t])
     surv_NBreed4[t+1] ~ dbin(s_BN[4], NBreed[4,t])"
    
    However I also keep checking other related equations and trying to solve this error for the model could be run trought for the next step.
    
    In this case If you have instructions and/or suggestions please let me known.
    
    Best Regards
    Ghan Saridnirun

--- GPeafowlIPM_CoreModel.R
+++ GPeafowlIPM_CoreModel.R
@@ -8,17 +8,17 @@ set.seed(mySeed)
 
 #Month order and season for data
 
-month <- factor(c("Nov19","Dec19",
-                  "Jan20","Feb20","Mar20","Apr20","May20","Jun20","Jul20","Aug20",
-                  "Sep20","Oct20","Nov20","Dec20","Jan21","Feb21","Mar21","Apr21",
-                  "May21","Jun21","Jul21","Aug21","Sep21","Oct21","Nov21","Dec21"
-),ordered = TRUE)
-
-season <- factor(c("Breeding","Breeding","Breeding","Breeding","Breeding",
-                   "Breeding","Non","Non","Non","Non","Non","Non","Breeding","Breeding",
-                   "Breeding","Breeding","Breeding","Breeding","Non","Non","Non","Non",
-                   "Non","Non","Breeding","Breeding"
-),ordered = TRUE)
+# month <- factor(c("Nov19","Dec19",
+#                   "Jan20","Feb20","Mar20","Apr20","May20","Jun20","Jul20","Aug20",
+#                   "Sep20","Oct20","Nov20","Dec20","Jan21","Feb21","Mar21","Apr21",
+#                   "May21","Jun21","Jul21","Aug21","Sep21","Oct21","Nov21","Dec21"
+# ),ordered = TRUE)
+# 
+# season <- factor(c("Breeding","Breeding","Breeding","Breeding","Breeding",
+#                    "Breeding","Non","Non","Non","Non","Non","Non","Breeding","Breeding",
+#                    "Breeding","Breeding","Breeding","Breeding","Non","Non","Non","Non",
+#                    "Non","Non","Breeding","Breeding"
+# ),ordered = TRUE)
 
 
 # Count in Non-Breeding Season
@@ -39,55 +39,48 @@ Br_BN_yr <- c(1,1,1,1,1,1,2,2,2,2,2,2,3,3)           #Season Label for breeder c
 Br_BN <- c(9,23,32,25,12,10,14,9,11,20,13,27,0,0)    #Breeder count in Breeding
 
 
-# Single Female count data
-SF_BN <- c(9,26,48,46,35,34,16,16,27,34,35,70,9,1)   #Single Female count in Breeding 
-SF_NB <- c(36,35,51,30,30,8,72,40,24,33,20,5)        #Single Female count in Non-Breeding         
-
-
-# Male count Data
-
-ChM_NB <- c(20,39,78,72,52,21,25,26,33,54,13,4)      #Male Chicks Count
-JuM_BN <- c(13,17,28,29,8,8,20,9,10,36,11,19,11,0)   #Male juveniel count
-
-M1y_BN <- c(4,16,11,19,13,18,8,2,9,14,16,28,1,0)     #Male 1 year count in Breeding   
-M1y_NB <- c(9,6,9,13,5,5,12,19,14,10,5,10)           #Male 1 year count in Non-Breeding
-
-M2y_BN <- c(1,6,4,6,2,6,4,6,1,8,2,14,0,0)            #Male 2 years count in Breeding
-M2y_NB <- c(5,1,8,4,0,2,2,1,0,0,0,1)                 #Male 2 years count in Non-Breeding
-
-M3y_BN <- c(27,118,164,119,49,57,54,60,76,91,111,83) #Male 3 yeras count in Breeding
-M3y_NB <- c(75,57,143,139,85,34,71,63,83,82,56,37)   #Male 3 years count in Non-Breeding
+# # Single Female count data
+# SF_BN <- c(9,26,48,46,35,34,16,16,27,34,35,70,9,1)   #Single Female count in Breeding 
+# SF_NB <- c(36,35,51,30,30,8,72,40,24,33,20,5)        #Single Female count in Non-Breeding         
+# 
+# 
+# # Male count Data
+# 
+# ChM_NB <- c(20,39,78,72,52,21,25,26,33,54,13,4)      #Male Chicks Count
+# JuM_BN <- c(13,17,28,29,8,8,20,9,10,36,11,19,11,0)   #Male juveniel count
+# 
+# M1y_BN <- c(4,16,11,19,13,18,8,2,9,14,16,28,1,0)     #Male 1 year count in Breeding   
+# M1y_NB <- c(9,6,9,13,5,5,12,19,14,10,5,10)           #Male 1 year count in Non-Breeding
+# 
+# M2y_BN <- c(1,6,4,6,2,6,4,6,1,8,2,14,0,0)            #Male 2 years count in Breeding
+# M2y_NB <- c(5,1,8,4,0,2,2,1,0,0,0,1)                 #Male 2 years count in Non-Breeding
+# 
+# M3y_BN <- c(27,118,164,119,49,57,54,60,76,91,111,83) #Male 3 yeras count in Breeding
+# M3y_NB <- c(75,57,143,139,85,34,71,63,83,82,56,37)   #Male 3 years count in Non-Breeding
 
 ny=20  # Length = Number of year following Green Peafowl age
 
 
 ## Arrange constants
 
-GP.IPMconstants <- list(Tmax = ny, Amax = 4 # A = Age class: 1.Chick/Juvenile,
+GP.IPMconstants <- list(Tmax = ny, Amax = 4, # A = Age class: 1.Chick/Juvenile,
                         #S = 2, #Y = 3          2.sub adult 1, 3.sub adult 2
                         #M = ?            # S = Seasons: 1.Breeding, 2.Non-Breeding
                         # Y = Year: 1, 2, 3
-                        # M = Month
+                        # M = Month              
+                        ChF_NB_yr=ChF_NB_yr,
+                        Br_NB_yr=Br_NB_yr,
+                        JuF_BN_yr=JuF_BN_yr,
+                        Br_BN_yr=Br_BN_yr
 )
 
 ## Arrange data
 
-  # GP.IPMdata  <- list(Chick = ChF_NB,
-  #               Juvenile = JuF_BN,
-  #               Breeder_NonBreeding = Br_NB,
-  #               Breeder_Breeding = Br_BN
-  #               #year label Non-Breeding = ChF_NB_yr, Br_NB_yr
-  #               #year label Breeding = JuF_BN_yr, Br_BN_yr
-  #               )
 
   GP.IPMdata <- list( 
-              ChF_NB_yr=ChF_NB_yr,  
               ChF_NB=ChF_NB,  
-              Br_NB_yr=Br_NB_yr,  
               Br_NB=Br_NB,  
-              JuF_BN_yr=JuF_BN_yr,  
               JuF_BN=JuF_BN,  
-              Br_BN_yr=Br_BN_yr,  
               Br_BN=Br_BN 
               ) 
 
@@ -134,7 +127,9 @@ GP.IPMconstants <- list(Tmax = ny, Amax = 4 # A = Age class: 1.Chick/Juvenile,
     }
     
     # Book-keeping
-    Nnon[1:4,1] <- 0
+    NNon[1:Amax,1] <- 0   
+    
+    
     
     # Process model: Breeding -> Non-Breeding season transition
     for (t in 1:ny){
@@ -162,40 +157,27 @@ GP.IPMconstants <- list(Tmax = ny, Amax = 4 # A = Age class: 1.Chick/Juvenile,
       }
       
       
-      # This section I think I just forget to add p[t] in the right side 
-      # due to it would provide the estimated probability of detection
-      
-      # #Observation Model in Non-Breeding
-      # ChF_NB[t] ~ dpois(p[t] * NNon[1,t])
-      # Br_NB[t] ~ dpois(p[t] * NNon[4,t])
-      # 
-      # 
-      # #Observation Model in Breeding
-      # JuF_BN[t] ~ dpois(p[t] * NBreed[2,t])
-      # Br_BN[t] ~ dpois(p[t] * NBreed[4,t])
-      
-      
       # # Observation Model in Non-Breeding
-      # for(j in 1:12) {
-      # 
-      #   ChF_NB[j] ~ dpois(p[t] * NNon[1,ChF_NB_yr[j]]) 
-      #   Br_NB[j] ~ dpois(p[t] * NNon[4,Br_NB_yr[j]]) 
-      # 
-      # }
-      # 
+      for(j in 1:12) {
+
+        ChF_NB[j] ~ dpois(p[t] * NNon[1,ChF_NB_yr[j]])
+        Br_NB[j] ~ dpois(p[t] * NNon[4,Br_NB_yr[j]])
+
+      }
+
       # # Observation Model in Breeding
-      # for(h in 1:14) {
-      # 
-      #   JuF_BN[h] ~ dpois(p[t] * NBreed[1,JuF_BN_yr[h]]) 
-      #   Br_BN[h] ~ dpois(p[t] * NBreed[4,Br_BN_yr[h]]) 
-      #   
-      # }
+      for(h in 1:12) {
+
+        JuF_BN[h] ~ dpois(p[t] * NBreed[1,JuF_BN_yr[h]])
+        Br_BN[h] ~ dpois(p[t] * NBreed[4,Br_BN_yr[h]])
+
+      }
     } 
-  }  
+  }
   )
   
   # Initial values
-  source("GPeafowlIPM_InitialSim.R")
+  source("TestMatrixGP.R")
   
   Inits <- GP_IPM_Init(Tmax = 20)
   Inits
@@ -207,13 +189,31 @@ GP.IPMconstants <- list(Tmax = ny, Amax = 4 # A = Age class: 1.Chick/Juvenile,
   
   
   # MCMC settings
-  ni <- 10
-  nb <- 0
-  nt <- 1
-  #nc <- 3
-  nc <- 1
   
-  # Run
+  # ni <- 10
+  # nb <- 0
+  # nt <- 1
+  # #nc <- 3
+  # nc <- 1
+  
+  # ni <- 200000     # Run Time around 15 minutes
+  # nb <- 50000      # Using initial values
+  # nt <- 30
+  # nc <- 4
+  
+  # ni <- 500000     # Run Time around 30 minutes
+  # nb <- 50000      # Using initial values
+  # nt <- 30
+  # nc <- 4
+  
+  ni <- 1000
+  nb <- 0
+  nt <- 30
+  nc <- 4
+  
+  
+  
+ # Run
   out <- nimbleMCMC(code = GP.IPMcode,
                     data = GP.IPMdata,
                     constants = GP.IPMconstants,
@@ -229,9 +229,38 @@ print(out,2)
 
 
 
+library(MCMCvis)
+
+  MCMCsummary(out, params = 'all', round = 2)
+
+  MCMCsummary(out, params = 'NBreed', round = 2)
+
+  MCMCsummary(out, params = 'NNon', round = 2)
+
+  MCMCsummary(out, params = 'Fec', round = 2)
+
+  MCMCsummary(out, params = 'rho', round = 2)
+
+  MCMCsummary(out, params = 'p', round = 2)
+
+  MCMCsummary(out, 
+            params = c('s_NB','s_BN','gamma','mean.rho',
+                       'sigma.rho','mean.p','sigma.p'),
+            round = 2)
 
 
 
+# Trace Plot
+
+  MCMCtrace(out, params = 'NBreed',
+          pdf = FALSE,
+          Rhat = TRUE,
+          n.eff = TRUE)
+
+  MCMCtrace(out, params = 'NNon',
+          pdf = FALSE,
+          Rhat = TRUE,
+          n.eff = TRUE)
 
 
 
