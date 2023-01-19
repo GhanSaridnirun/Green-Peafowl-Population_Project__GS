9c2ad89cd670ee22129eb106d53c011e1fafd7a7
ChloeRNater <chloe.nater@nina.no>
2023-01-19 14:54:28 +0100

    Added matrix prototype code

---
+++ R/Prototype_GPMatrix.R
@@ -0,0 +1,23 @@
+
+# Forming matrix
+
+
+N <- matrix(0, nrow=16, ncol=16)
+dimnames(N) <- list(c("ChF_NB", "ChM_NB", "JuF_BN", "JuM_BN",
+                      "1yF_NB", "1yM_NB", "1yF_BN", "1yM_BN",
+                      "2yF_NB", "2yM_NB", "2yF_BN", "2yM_BN",
+                      "3yF_NB", "3yM_NB", "3yF_BN", "3yM_BN"),
+                    
+                    c("ChF_NB", "ChM_NB", "JuF_BN", "JuM_BN",
+                      "1yF_NB", "1yM_NB", "1yF_BN", "1yM_BN",
+                      "2yF_NB", "2yM_NB", "2yF_BN", "2yM_BN",
+                      "3yF_NB", "3yM_NB", "3yF_BN", "3yM_BN"))
+
+
+pop.mat <- N
+pop.mat
+
+pop.mat.num <- pop.mat
+
+
+
