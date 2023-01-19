d246794759ee75a816e5da169f6bc35220829f0c
ChloeRNater <chloe.nater@nina.no>
2023-01-19 12:22:08 +0100

    Updated comparison of N estimates across models

--- R/Model_Comparison.R
+++ R/Model_Comparison.R
@@ -1,6 +1,3 @@
-
-# Test Run 
-
 library(coda)
 library(ggplot2)
 library(reshape2)
@@ -9,52 +6,32 @@ library(tidyr)
 library(plyr)
 
 
+IPM.rhoEst <- readRDS("GPIPM_TwoSex_Matrix_Clutch_BreedProb_rhoEst.rds")
+IPM.rhoDeriv <- readRDS("GPIPM_TwoSex_Matrix_Clutch_BreedProb_rhoDeriv.rds")
 
 
-rhopRep.IPM <- readRDS('[A__PeafowlIPM_rhoxpRep.rds')
-ChSpRep.IPM <- readRDS('[A__PeafowlIPM_CSxpRep.rds')
-rhoprior.IPM <- readRDS('[A__PeafowlIPM_rhoxpRep_rho_onlyprior.rds')
-Choutpro.IPM <- readRDS('[A__PeafowlIPM_pRepxNoCSinProcess.rds')
-
-
-
-out.mat <- list(rhpR = as.matrix(rhopRep.IPM),
-                ChpR = as.matrix(ChSpRep.IPM),
-                rhpr = as.matrix(rhoprior.IPM),
-                chou = as.matrix(Choutpro.IPM)
-                
+out.mat <- list(rhoEst = as.matrix(IPM.rhoEst),
+                rhoDeriv = as.matrix(IPM.rhoDeriv)
 )
 
 
 
-## Set the study years for each population
+## Set the range of study years
 
-StudyYears <- list(
-  rhpR = 2019:2072,
-  ChpR = 2019:2072,
-  rhpr = 2019:2072,
-  chou = 2019:2072
-)
+StudyYears <- 2019:2072
 
 
 
-## Extract Tmax for all models\
+## Extract Tmax 
 
-ModelTmax <- c(length(StudyYears$rhpR),
-               length(StudyYears$ChpR),
-               length(StudyYears$rhpr),
-               length(StudyYears$chou)
-)
+ModelTmax <- length(StudyYears)
 
 
 ## Make an empty list for storing estimates for each model
 
 sum.data <- list(
-  rhpR = NA,
-  ChpR = NA,
-  rhpr = NA,
-  chou = NA
-  
+  rhoEst = NA,
+  rhoDeriv = NA
 )
 
 
@@ -68,7 +45,7 @@ sam.summary <- function(x){
 
 ## For each model, extract relevant measures
 
-ModelIDs <- c('rho x BreedpRob', 'Clutch x BreedpRob', 'rho.inProcess', 'Clutch.onlyPrior')
+ModelIDs <- c("rho estimated", "rho derived from CS")
 
 sum.list <- list()
 
@@ -82,27 +59,35 @@ for(i in 1:length(ModelIDs)){
   
   
   # Prepare matrices to store results
-  NTOT <- matrix(NA, nrow = ModelTmax[i], ncol = 3, dimnames = list(NULL, c('lCI', 'Median', 'uCI')))
+  NTOT_Breed <- NTOT_Non <- matrix(NA, nrow = ModelTmax, ncol = 3, dimnames = list(NULL, c('lCI', 'Median', 'uCI')))
   
-  
-  # Make posterior summary for NTOT in each year
-  for(t in 1:ModelTmax[i]){
+ 
+  for(t in 1:ModelTmax){
     
-    NTOT[t,] <- sam.summary(sam.mat[,paste0('NTOT[', t, ']')])
+    # Calclulate NTOT in both seasons
+    N_Breed <- rowSums(sam.mat[,paste0('NBreedF[', 1:4, ", ", t, ']')]) + 
+               rowSums(sam.mat[,paste0('NBreedM[', 1:4, ", ", t, ']')])
     
+    N_Non <- rowSums(sam.mat[,paste0('NNonF[', 1:4, ", ", t, ']')]) + 
+             rowSums(sam.mat[,paste0('NNonM[', 1:4, ", ", t, ']')])
+    
+    
+    # Make posterior summary for NTOT in each year
+    NTOT_Breed[t,] <- sam.summary(N_Breed)
+    NTOT_Non[t,] <- sam.summary(N_Non)
   }
   
   
   # Organise summaries in a data frame
   model.sum.data <- data.frame(
     ModelID = ModelIDs[i],
-    Year = StudyYears[[i]],
-    Parameter = rep(c('NTOT'), each = ModelTmax[i])
+    Year = StudyYears,
+    Parameter = rep(c('NTOT_Breed', 'NTOT_Non'), each = ModelTmax[i])
   )
   
   model.sum.data <- cbind(
     model.sum.data,
-    rbind(NTOT))
+    c(NTOT_Breed, NTOT_Non))
   
   # Store summary data
   sum.list[[i]] <- model.sum.data
@@ -119,12 +104,12 @@ allModel.data <- dplyr::bind_rows(sum.list, .id = "column_label")
 
 ## Plot Population sizes
 
-ggplot(allModel.data, aes(x = Year, y = Median)) +
-  geom_line(aes(color = ModelID, linetype = Parameter)) +
-  geom_ribbon(aes(ymin = lCI, ymax = uCI, fill = ModelID),alpha = 0.2) +
+ggplot(allModel.data, aes(x = Year, y = Median, group = ModelID)) +
+  geom_line(aes(color = ModelID)) +
+  geom_ribbon(aes(ymin = lCI, ymax = uCI, fill = ModelID), alpha = 0.2) +
   ggtitle('Total Population Size') + 
   ylab('Estimate') + 
-  # facet_wrap(~ModelID, ncol = 1, scales = 'free_y') +
+  facet_wrap(~Parameter, ncol = 1, scales = 'free_y') +
   theme_bw() + theme(legend.position = "right", panel.grid.major.y = element_blank()
                    , panel.grid.minor.y = element_blank(),
                    axis.text.x = element_text(angle = 45, vjust = 0.5),
