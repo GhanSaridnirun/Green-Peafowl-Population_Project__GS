7f3c5e8032a088212ca1e469af2ba933e6f78592
Ghan Saridnirun <g.saridnirun@gmail.com>
2023-01-22 12:33:21 +0100

    Update numerical matrix
    - Add number assuagements
    - Change some vector assignments for code running.
    - The New code place below the assignment task
    - I experience the problem when I tried to run difference model from differed parameter setting I got all values that I should excepted sensitivity and elasticity that appeared as NA.
    - I have disable the code that assigned the Chick produce from F1Y due to 
    I remember that the 2 year females F2Y and 3 year up females F3y  could provided the offspring while F1Y still immature to do that, I just noticed that I must forgot this point when we are constructing the matrix.

--- R/Prototype_GPMatrix.R
+++ R/Prototype_GPMatrix.R
@@ -3,6 +3,13 @@
 # FULL MATRIX #
 ###############
 
+# Number Assignment 
+
+
+
+
+
+
 N <- matrix(0, nrow = 16, ncol = 16)
 dimnames(N) <- list(c("ChF_NB", "ChM_NB", "JuF_BN", "JuM_BN",
                       "1yF_NB", "1yM_NB", "1yF_BN", "1yM_BN",
@@ -142,6 +149,127 @@ ann.pop.mat["3yM", "3yM"] <- "sM_NB[4] * sM_BN[4]"
 # - Make numerical matrices
 # - Calculate asymptotic population growth rate = dominant right eigenvalue of the matrix using eigen()
 # - Compare pop. growth rate: should be the same
+
+
+###########################
+## Applying the numberic ##
+###########################
+
+# Vital rates
+
+# Productivity 
+
+pRep <- 0.5
+mean.CS <- 5
+gamma <- 0.5
+S_C <- 0.5
+
+# Survival rate 
+
+sFCh_NB <- 0.5  # Female Chick survival Non-breeding
+sMCh_NB <- 0.5  # Male Chick survival Non-breeding
+sFCh_BN <- 0.5  # Female Juvenile survival Breeding
+sMCh_BN <- 0.5  # Male Juvenile survival Breeding
+
+sF1y_NB <- 0.6  # Female Yearling survival Non-breeding 
+sM1y_NB <- 0.6  # Male Yearling survival Non-breeding 
+sF1y_BN <- 0.6  # Female Yearling survival Breeding
+sM1y_BN <- 0.6  # Male Yearling survival Breeding
+
+sF2y_NB <- 0.7  # Female 2 years old survival Non-breeding 
+sM2y_NB <- 0.7  # Male 2 years old survival Non-breeding 
+sF2y_BN <- 0.7  # Female 2 years old survival Breeding
+sM2y_BN <- 0.7  # Male 2 years old survival Breeding
+
+sF3y_NB <- 0.8  # Female 3 years old survival Non-breeding 
+sM3y_NB <- 0.8  # Male 3 years old survival Non-breeding 
+sF3y_BN <- 0.8  # Female 3 years old survival Breeding
+sM3y_BN <- 0.8  # Male 3 years old survival Breeding
+
+
+
+N <- matrix(0, nrow = 8, ncol = 8)
+dimnames(N) <- list(c("ChF", "ChM",
+                      "1yF", "1yM",
+                      "2yF", "2yM",
+                      "3yF", "3yM"),
+                    
+                    c("ChF", "ChM",
+                      "1yF", "1yM",
+                      "2yF", "2yM",
+                      "3yF", "3yM"))
+
+
+ann.pop.mat <- N
+ann.pop.mat
+
+ann.pop.mat.num <- ann.pop.mat
+
+
+# Row index = gets produced
+# Column index = produces
+
+# Female chicks (NB)
+# ann.pop.mat["ChF", "1yF"] <- sF1y_NB * pRep * mean.CS * S_C * gamma
+ann.pop.mat["ChF", "2yF"] <- sF2y_NB * pRep * mean.CS * S_C * gamma
+ann.pop.mat["ChF", "3yF"] <- sF3y_NB * pRep * mean.CS * S_C * gamma
+
+
+# Male chicks (NB)
+# ann.pop.mat["ChM", "1yF"] <- sF1y_NB * pRep * mean.CS * S_C * (1-gamma)
+ann.pop.mat["ChM", "2yF"] <- sF2y_NB * pRep * mean.CS * S_C * (1-gamma)
+ann.pop.mat["ChM", "3yF"] <- sF3y_NB * pRep * mean.CS * S_C * (1-gamma)
+
+
+# Female yearling (NB)
+ann.pop.mat["1yF", "ChF"] <- sFCh_NB * sFCh_BN
+
+# Male yearling (NB)
+ann.pop.mat["1yM", "ChM"] <- sMCh_NB * sMCh_BN
+
+# Female 2-yr (NB)
+ann.pop.mat["2yF", "1yF"] <- sF1y_NB * sF1y_BN
+
+# Male 2-yr (NB)
+ann.pop.mat["2yM", "1yM"] <- sM1y_NB * sM1y_BN
+
+# Female 3-yr (NB)
+ann.pop.mat["3yF", "2yF"] <- sF2y_NB * sF2y_BN
+ann.pop.mat["3yF", "3yF"] <- sF3y_NB * sF3y_BN
+
+# Male 3-yr (NB)
+ann.pop.mat["3yM", "2yM"] <- sM2y_NB * sM2y_BN
+ann.pop.mat["3yM", "3yM"] <- sM3y_NB * sM3y_BN
+
+
+# Calculate lambda (= dominant right eigenvalue)
+eigen(ann.pop.mat)$values[1]
+
+# Calculate stable age structure
+eigen(ann.pop.mat)$vectors[,1] / sum(eigen(ann.pop.mat)$vectors[,1])
+
+# Using popbio package
+popbio::eigen.analysis(ann.pop.mat)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
