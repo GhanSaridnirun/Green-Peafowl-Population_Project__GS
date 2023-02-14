adfbb471182de769ca54c8cd6a829aa8fcb519b5
ChloeRNater <chloe.nater@nina.no>
2023-02-14 15:11:38 +0100

    Set up skeleton scripts for sensitivity analysis
    Two versions: "Matrix" = conceptual, using single dummy values. "IPM" = iterative, using posterior samples of (time-average) vital rates from the IPM.

---
+++ R/GPeafowlIPM_SensElasAnalysis.R
@@ -0,0 +1,39 @@
+########################
+# GREEN PEAFOWL IPM    #
+# SENSITIVITY ANALYSIS #
+########################
+
+
+# 1. Set parameter values #
+#-------------------------#
+
+
+
+# 2. Build projection matrix #
+#----------------------------#
+
+
+
+# 3. Calculate asymptotic population growth rate #
+#------------------------------------------------#
+
+
+
+# 4. Calculate matrix element sensitivities #
+#-------------------------------------------#
+
+
+
+# 5. Calculate matrix element elasticities #
+#------------------------------------------#
+
+
+
+# 6. Calculate vital rate sensitivities #
+#---------------------------------------#
+
+
+
+# 7. Calculate vital rate elasticieites #
+#---------------------------------------#
+
