d3c9d5d09d9595146e98171c017e49b9dc0e1fd4
ChloeRNater <chloe.nater@nina.no>
2023-02-15 12:24:44 +0100

    Set up example for sensitivity and elasticity analysis

--- R/GPeafowlMatrix_SensElasAnalysis.R
+++ R/GPeafowlMatrix_SensElasAnalysis.R
@@ -7,33 +7,217 @@
 # 1. Set parameter values #
 #-------------------------#
 
+## Productivity 
+pRep <- 0.9 # Female breeding probability
+mean.CS <- 8 # Average clutch size
+gamma <- 0.5 # Sex ratio at birth
+S_C <- 0.7 # Clutch survival
+
+## Survival
+sF_NB <- c(0.55, 0.65, 0.75, 0.9) # Female survival Non-breeding -> Breeding (chicks, 1yr, 2yr, 3yr)
+sF_BN <- c(0.55, 0.65, 0.75, 0.9) # Female survival Breeding -> Non-breeding (juveniles, 1yr, 2yr, 3yr)
+sM_NB <- c(0.55, 0.65, 0.75, 0.9) # Male survival Non-breeding -> Breeding (chicks, 1yr, 2yr, 3yr)
+sM_BN <- c(0.55, 0.65, 0.75, 0.9) # Male survival Breeding -> Non-breeding (juveniles, 1yr, 2yr, 3yr)
+
 
 
 # 2. Build projection matrix #
 #----------------------------#
+
+## Function to make projection matrix
+source("R/make.GPprojMatrix.R")
+
+## Build annual matrix
+mat.ann <- make.GPprojMatrix(pRep = pRep, mean.CS = mean.CS, S_C = S_C, gamma = gamma, 
+                             sF_NB = sF_NB, sF_BN = sF_BN, sM_NB = sM_NB, sM_BN = sM_BN,
+                             seasonal = FALSE)
+
+
+## Build seasonal matrix
+mat.season <- make.GPprojMatrix(pRep = pRep, mean.CS = mean.CS, S_C = S_C, gamma = gamma, 
+                                sF_NB = sF_NB, sF_BN = sF_BN, sM_NB = sM_NB, sM_BN = sM_BN,
+                                seasonal = TRUE)
 
 
 
 # 3. Calculate asymptotic population growth rate #
 #------------------------------------------------#
 
+## Set initial population to use in simulations
+N1.ann <- c(100, 100, # Chicks
+           25, 22, # 1 yr olds
+           20, 15, # 2yr olds
+           50, 40) # 3+yr olds
+
+N1.season <- c(N1.ann[1:2],
+              0, 0,
+              N1.ann[3:4],
+              0, 0,
+              N1.ann[5:6],
+              0, 0,
+              N1.ann[7:8],
+              0, 0)
+# NOTE: The 0's in the seasonal population are for the stages not present in the first season.
+# Since we'll consider an annual census at the start of the breeding season, there will be 0
+# individuals of all the non-breeding season stages
+
+
+## Annual matrix
+# Using eigen decomposition
+lambda.ann <- eigen(mat.ann$A)$values[1]
+lambda.ann
+# --> Just above one, meaning population growing by about 1.3% per year (under stable conditions)
+
+# Using simulation
+tSim <- 200
+N.sim.ann <- matrix(NA, nrow = length(N1.ann), ncol = tSim)
+N.sim.ann[, 1] <- N1.ann
+
+for(t in 1:(tSim-1)){
+  N.sim.ann[, t+1] <- mat.ann$A %*% N.sim.ann[, t]
+}
+matplot(t(N.sim.ann), type = "l") # Population by stage
+plot(colSums(N.sim.ann), type = "l") # Entire population
+plot(log(colSums(N.sim.ann)), type = "l") # Entire population (log)
+
+lambda.ann.sim <- sum(N.sim.ann[,tSim]) / sum(N.sim.ann[,tSim-1])
+
+# Comparison
+lambda.ann
+lambda.ann.sim
+# --> They are basically identical (as they should be)
+
+
+## Seasonal matrices
+# Using eigen decomposition
+lambda.BtoN <- eigen(mat.season$A_BtoN)$values[1]
+lambda.BtoN 
+lambda.NtoB <- eigen(mat.season$A_NtoB)$values[1]
+lambda.NtoB
+# --> Both are 0 because life cycle is "interrupted" when considering seasons separately
+
+# Using simulation
+N.sim.season <- matrix(NA, nrow = length(N1.season), ncol = tSim*2) # Twice as many time steps since we have two seasons
+N.sim.season[, 1] <- N1.season
+
+for(t in 1:((tSim*2)-1)){
+  
+  # Non-breeding to breeding season
+  if(t %% 2 == 1){ # If t is odd ...
+    N.sim.season[, t+1] <- mat.season$A_NtoB %*% N.sim.season[, t]
+  } 
+  
+  # Breeding to non-breeding season
+  if(t %% 2 == 0){ # If t is even ...
+    N.sim.season[, t+1] <- mat.season$A_BtoN %*% N.sim.season[, t]
+  } 
+
+}
+
+matplot(t(N.sim.season), type = "l") # Population by stage (both seasons)
+plot(colSums(N.sim.season), type = "l") # Entire population (both seasons)
+plot(log(colSums(N.sim.season)), type = "l") # Entire population (log, both seasons)
+
+N.sim.NB <- N.sim.season[, which(1:(tSim*2) %% 2 == 1)]
+matplot(t(N.sim.NB), type = "l") # Population by stage (both seasons)
+plot(colSums(N.sim.NB), type = "l") # Entire population (both seasons)
+plot(log(colSums(N.sim.NB)), type = "l") # Entire population (log, both seasons)
+
+lambda.season.sim <- sum(N.sim.NB[,tSim]) / sum(N.sim.NB[,tSim-1])
+
+
+## Compare (if matrices are specified correctly, they should have the same lambda)
+lambda.ann.sim == lambda.season.sim
+plot(colSums(N.sim.ann), type = "l")
+lines(colSums(N.sim.ann), col = "red")
+# --> They are the same, so our matrices are correct.
+# --> Therefore we can safely use the simpler, annual matrix in the following
+
 
 
 # 4. Calculate matrix element sensitivities #
 #-------------------------------------------#
+
+## Using the popbio package
+popbio::eigen.analysis(mat.ann$A)
+# --> This approach gives us NA for sensitivities
+# --> The reason for that is that our projection matrix is singular, and its inverse (necessary for sensitivity calculations) not defined
+
+## Using perturbation analysis
+# NOTE: Sensitivities (and elasticities) can also be calculated using perturbation analysis instead of the analytical approach implemented in popbio
+#       The perturbation approach will also work for singular matrices, and is very straightforward to apply also for
+#       calculating sensitivities/elasticities with respect to vital rates (below).
+#       Additionally, you can use it not just for sensitivities/elasticities of asymptotic population growth rate, but for any metric of your choice.
+
+# Set perturbation factor
+dy <- 1e-5
+
+# Set up original and perturbed matrix
+A_orig <- mat.ann$A
+A_pert <- A_orig
+
+# Perturb target element in matrix
+A_pert[3, 1] <- A_pert[3, 1] + dy
+
+# Calculate population growth rate for both matrices
+lam_orig <- as.numeric(eigen(A_orig)$values[1])
+lam_pert <- as.numeric(eigen(A_pert)$values[1])
+
+# Calculate sensitivity of population growth rate to target element
+sens <- matrix(NA, nrow = nrow(A_orig), ncol = ncol(A_orig))
+sens[3, 1] <- (lam_pert - lam_orig) / dy
+
+# TODO: 
+# - Generalize above code to calculate sensitivities for all (non-0) matrix elements
+# - Visualize sensitivity matrix (tip: start with fields::image.plot)
+
 
 
 
 # 5. Calculate matrix element elasticities #
 #------------------------------------------#
 
+## Using perturbation analysis
+# Set perturbation factor
+dy <- 1e-5
+
+# Set up original and perturbed matrix
+A_orig <- mat.ann$A
+A_pert <- A_orig
+
+# Perturb target element in matrix
+A_pert[3, 1] <- A_pert[3, 1] * (1 + dy)
+
+# Calculate population growth rate for both matrices
+lam_orig <- as.numeric(eigen(A_orig)$values[1])
+lam_pert <- as.numeric(eigen(A_pert)$values[1])
+
+# Calculate sensitivity of population growth rate to target element
+elas <- matrix(NA, nrow = nrow(A_orig), ncol = ncol(A_orig))
+elas[3, 1] <- (lam_pert - lam_orig) / (lam_orig * dy)
+
+# TODO: 
+# - Generalize above code to calculate sensitivities for all (non-0) matrix elements
+# - Visualize elasticity matrix (tip: start with fields::image.plot)
+
 
 
 # 6. Calculate vital rate sensitivities #
 #---------------------------------------#
 
+## Using perturbation analysis
+# TODO: 
+# - Adapt the above code to calculate sensitivities with respect to vital rates (instead of matrix elements)
+#   NOTE: Keep in mind that unlike matrix elements, vital rates may appear several times in the matrix!
+# - Generalize your code to calculate sensitivities for all vital rates
+# - Visualize sensitivities for all vital rates 
+
 
 
 # 7. Calculate vital rate elasticieites #
 #---------------------------------------#
+
+## Using perturbation analysis
+# TODO: 
+# - Analogous to 6., but using formulas for elasticity (see 5.)
 
